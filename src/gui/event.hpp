fde7cbd6276f3587b42bd3bd8749cc0cbd9bacd5
Gabriel Majeri <gabriel.majeri6@gmail.com>
2019-04-27 08:52:40 +0300

    Remove EventLoop class
    UI components will automatically receive all events and handle
    the ones they want.

--- src/gui/event.hpp
+++ src/gui/event.hpp
@@ -1,36 +1,8 @@
 #pragma once
 
-#include <functional>
-#include <unordered_map>
-
 #include "core/sdl.hpp"
 
-/// Interface to the main event loop of the application.
-///
-/// This class should only be used on the main thread, and be treated as
-/// a singleton, since multiple instances of it will conflict.
-class EventLoop {
-    using EventHandler = std::function<void(const SDL_Event& event)>;
-    using EventHandlerMap = std::unordered_map<unsigned, EventHandler>;
-    EventHandlerMap handlers;
-
-    bool running;
-
+class EventHandler {
 public:
-    /// Initializes the event loop which stops when the user quits the app.
-    EventLoop();
-
-    /// Sets a handler for an SDL event type.
-    void setEventHandler(SDL_EventType type, EventHandler&& handler);
-
-    /// Start the event loop.
-    ///
-    /// The event loop keeps pumping events until stop has been requested.
-    void run();
-
-    /// Stop the event loop from handling more events.
-    ///
-    /// This will finish processing the current iteration and then
-    /// the event loop will exit.
-    void requestStop();
+    virtual void handleEvent(const SDL_Event& e);
 };
